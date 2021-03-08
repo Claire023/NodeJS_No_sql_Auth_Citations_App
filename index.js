6422e013114c20901ce87d3a5d3d31db88444175
Claire Monnier <claire.monnier@ucit.fr>
2021-03-08 08:48:35 +0100

    auth done

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const path = require('path');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
 const mongoose = require('mongoose');
-const Quote = require('./models/quote');
+const ExpressError = require('./utils/ExpressError');
+const passport = require('passport');
+const flash = require('connect-flash');
+const LocalStrategy = require('passport-local');
+const User =  require('./models/user');
 
+const userRoutes = require('./routes/users');
+const quoteRoutes = require('./routes/quotes');
+const {isLoggedIn} = require('./middleware');
 
 mongoose.connect('mongodb://localhost:27017/quotes',{
     useNewUrlParser:true,
@@ -22,67 +31,54 @@ db.once("open", ()=>{
 
 const app = express();
 
+//pour parser les templates
+app.engine('ejs', ejsMate);
+
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'))
 
 app.use(express.urlencoded({extended:true}));
+
+//pour les sessions
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+
+app.use(session(sessionConfig));
+//pour les messages d'erreur
+app.use(flash());
 //utiliser les put dans formulaire
 app.use(methodOverride('_method'));
 
 
-app.get('/', (req,res)=>{
-    res.render("Home");
+//Authentication
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req,res,next)=>{
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
 })
 
-// obtenir la liste des citations
-app.get('/quotes', async (req,res)=>{
-    const quotes = await Quote.find({});
-    // const q = new Quote({description:'my first quote', author:'Claire', book:'No book', year:'2021'});
-   res.render('quotes/index', {quotes})
-});
 
+//declaration des routes
+app.use('/', userRoutes);
+app.use('/quotes', quoteRoutes);
 
-  //acceder au form pour post
-  app.get('/quotes/new', (req,res)=>{
-    res.render('quotes/create')
- });
-
-
-
-//Créer une reservation
-app.post('/quotes', async(req,res)=>{
-    const quote = new Quote(req.body.quote);
-    await quote.save();
-    res.redirect(`quotes/${quote._id}`);
-});
-
-
-
-//details de la citation
-app.get('/quotes/:id', async(req,res)=>{
-    const quotes = await Quote.findById({_id:req.params.id})
-    res.render('quotes/details', {quotes});
-});
-
-
-app.get('/quotes/:id/edit', async(req,res)=>{
-    const quotes = await Quote.findById({_id:req.params.id})
-    res.render('quotes/edit',{quotes});
-});
-
-//petit bug de redirection mais sinon ca fonctionne
-app.put('/quotes/:id', async(req,res)=>{
-    const { id } = req.params;
-    const quote = await Quote.findByIdAndUpdate(id, { ...req.body.quote });
-    // res.redirect(`quotes/${quote._id}`)
-    res.redirect('/quotes');
-});
-
-app.delete('/quotes/:id', async(req,res)=>{
-    const { id } = req.params;
-    await Quote.findByIdAndDelete(id);
-    res.redirect('/quotes');
-});
 
 
 app.listen(3000, ()=>{
--- models/quote.js
+++ models/quote.js
@@ -6,7 +6,11 @@ const QuoteSchema = new mongoose.Schema({
     description:String,
     author:String,
     book:String,
-    year:Number
+    year:Number,
+    publisher:{
+        type:mongoose.Schema.Types.ObjectId, 
+        ref:'User'
+    }
 });
 
 module.exports = mongoose.model('Quote', QuoteSchema);
